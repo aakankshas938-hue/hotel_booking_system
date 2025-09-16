c9c0847d21bb70c8e85c5add4de42a66bef2e9d1
aakankshas938-hue <aakankshas938@gmail.com>
2025-09-17 00:43:03 +0530

    Updated booking and hotel_booking URLs and views

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -10,7 +10,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
 
-I# Remove 'django.contrib.admin' from INSTALLED_APPS
+# Remove 'django.contrib.admin' from INSTALLED_APPS
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hotel_booking/urls.py
+++ hotel_booking/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-
-# Temporary view to test if Django is working
-def test_view(request):
-    return HttpResponse("🏨 Hotel Booking System is working!")
 
 urlpatterns = [
-    path('', test_view),  # Temporary root view
-    path('', include('booking.urls')),  # Your booking app URLs
+    path('', include('booking.urls')),
 ]
 
+# Serve static and media files during development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
