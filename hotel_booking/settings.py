cd471857fb1330eca4c445cb2029dc9bb523397f
aakankshas938-hue <aakankshas938@gmail.com>
2025-09-17 18:37:49 +0530

    initial

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -18,7 +18,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking',
+    'cloudinary',
+    'cloudinary_storage',
 ]
+
+# Add at the bottom of settings.py
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config( 
+  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
+  api_key = os.environ.get('CLOUDINARY_API_KEY'), 
+  api_secret = os.environ.get('CLOUDINARY_API_SECRET')
+)
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
